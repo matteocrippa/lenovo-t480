4d0c2d27c628e48da9d1f4cabc0a747760a16bc3
Matteo Crippa <matteocrippa@users.noreply.github.com>
2019-06-23 11:40:12 +0200

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -13,5 +13,5 @@ export HOSTNAME=earth
 export TIMEZONE=/Europe/Rome
 export USER=matteo
 export BOOT_CFG=/boot/loader/entries/arch.conf
-export HOOKS="HOOKS=(base autodetect modconf block keymap keyboard sd-encrypt sd-lvm2 filesystems fsck systemd)"
+export HOOKS="HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block SD-encrypt sd-lvm2 filesystems fsck)" 
 
